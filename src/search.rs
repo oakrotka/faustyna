7806456ec319ec28ee5f050ed354ebb05e681445
krotka <oliwiapolonia@gmail.com>
2026-05-28 23:17:26 +0200

    heavily refactored searching
    - defined an enum to better differentiate possible search cases
    - switched to fail-soft pruning from fail-hard

--- src/search.rs
+++ src/search.rs
@@ -7,16 +7,15 @@ use crate::evaluation::evaluate;
 
 const MAX_PLY: u64 = u64::MAX;
 
-const TIME_TO_THINK: Duration = Duration::from_millis(100);
+const TIME_TO_THINK: Duration = Duration::from_millis(2000);
 const TIME_CHECK_FREQUENCY: u8 = 50;
 
 pub fn choose_move(board: Board) -> ChessMove {
     let start_time = Instant::now();
 
+    let mut best_choice: Option<ChessMove> = None;
+    let mut best_score = f64::NEG_INFINITY;
     let mut previous_state = board;
-
-    let mut choice = ChessMove::default();
-    let mut score = f64::NEG_INFINITY;
 
     for depth in 1..MAX_PLY {
         println!("info depth {depth}");
@@ -26,41 +25,53 @@ pub fn choose_move(board: Board) -> ChessMove {
 
         // searching the previously found best move first for better alpha pruning
         if depth > 1 {
-            (previous_state, _, score) = match search_moves(
+            // updating move stats for a greater depth
+            (best_score, previous_state) = match search_moves(
                 previous_state,
                 f64::INFINITY,
                 f64::NEG_INFINITY,
                 1,
                 &mut transposition_table,
                 &mut iterations,
-                start_time,
+                start_time
             ) {
-                Some(res) => res,
-                None => return choice,
+                SearchResult::BestMove(_, score, state) => (score, state),
+                SearchResult::OutOfTime | SearchResult::OutOfTimeWithResult(..) => break,
+                SearchResult::CacheHit(_) | SearchResult::Leaf(..) => {
+                    unreachable!("impossible with an empty transposition table and depth 1")
+                }
             };
 
-            if score == f64::INFINITY {
+            // checkmate guaranteed
+            if best_score == f64::INFINITY {
                 break;
             }
         }
 
         // search the tree thoroughly this time
-        let (found_state, found_move, found_score) = match search_moves(
+        match search_moves(
             board,
-            score,
+            best_score,
             f64::INFINITY,
             depth,
             &mut transposition_table,
             &mut iterations,
-            start_time,
+            start_time
         ) {
-            Some(res) => res,
-            None => return choice,
+            SearchResult::BestMove(choice, score, state) if score > best_score => {
+                best_choice = Some(choice);
+                best_score = score;
+                previous_state = state;
+            }
+            SearchResult::OutOfTimeWithResult(choice, score) if score > best_score => {
+                best_choice = Some(choice);
+                best_score = score;
+                break;
+            }
+            SearchResult::OutOfTimeWithResult(..) | SearchResult::OutOfTime => break,
+            SearchResult::BestMove(..) | SearchResult::CacheHit(_) => (),
+            SearchResult::Leaf(..) => unreachable!("impossible with depth >=1"),
         };
-
-        if found_score > score || depth == 1 {
-            (previous_state, choice, score) = (found_state, found_move, found_score);
-        }
 
         if previous_state.status() != BoardStatus::Ongoing || start_time.elapsed() >= TIME_TO_THINK
         {
@@ -68,12 +79,12 @@ pub fn choose_move(board: Board) -> ChessMove {
         }
     }
 
-    println!("info score cp {score}");
-    choice
+    println!("info score cp {best_score}");
+    best_choice.expect("no move was found")
 }
 
 /// Returns the best move found from the given position, its score and the state of the board at the
-/// deepest searched moment. May return None if time runs out.
+/// deepest searched moment.
 fn search_moves(
     board: Board,
     alpha: f64,
@@ -81,20 +92,24 @@ fn search_moves(
     depth: u64,
     transposition_table: &mut RapidHashMap<u64, f64>,
     iterations: &mut u8,
-    start_time: Instant,
-) -> Option<(Board, ChessMove, f64)> {
+    start_time: Instant
+) -> SearchResult {
+    // check the time every fixed amount of searches to avoid having to do a syscall every time
+    // (TODO: is this really faster?)
     if *iterations == 0 {
         if start_time.elapsed() >= TIME_TO_THINK {
-            return None;
+            return SearchResult::OutOfTime;
         }
         *iterations = TIME_CHECK_FREQUENCY;
     }
     *iterations -= 1;
 
-    if let Some(score) = transposition_table.get(&board.get_hash()) {
-        return Some((Board::default(), ChessMove::default(), *score));
+    // look for position in cache
+    if let Some(&score) = transposition_table.get(&board.get_hash()) {
+        return SearchResult::CacheHit(score);
     }
 
+    // descend deeper into the move tree
     let res = alpha_beta(
         board,
         alpha,
@@ -102,10 +117,11 @@ fn search_moves(
         depth,
         transposition_table,
         iterations,
-        start_time,
+        start_time
     );
 
-    if let Some((_, _, score)) = res {
+    // update the cache
+    if let Some(score) = res.score() {
         transposition_table.insert(board.get_hash(), score);
     }
 
@@ -119,45 +135,109 @@ fn alpha_beta(
     depth: u64,
     transposition_table: &mut RapidHashMap<u64, f64>,
     iterations: &mut u8,
-    start_time: Instant,
-) -> Option<(Board, ChessMove, f64)> {
+    start_time: Instant
+) -> SearchResult {
     if depth == 0 {
         let score = evaluate(board);
-        transposition_table.insert(board.get_hash(), score);
-        return Some((board, ChessMove::default(), score));
+        return SearchResult::Leaf(score, board);
     }
 
     match board.status() {
-        BoardStatus::Checkmate => Some((board, ChessMove::default(), f64::NEG_INFINITY)),
-        BoardStatus::Stalemate => Some((board, ChessMove::default(), 0.0)),
+        BoardStatus::Checkmate => SearchResult::Leaf(f64::NEG_INFINITY, board),
+        BoardStatus::Stalemate => SearchResult::Leaf(0.0, board),
         BoardStatus::Ongoing => {
-            let mut best_state = Board::default();
-            let mut best_move = ChessMove::default();
+            // if the game is ongoing, there has to be at least one move to be considered
+            let mut best_value: Option<SearchResult> = None;
+            let mut best_score = f64::NEG_INFINITY;
 
             for chess_move in MoveGen::new_legal(&board) {
-                let (found_state, _, neg_score) = search_moves(
+                let (findings, score) = match search_moves(
                     board.make_move_new(chess_move),
                     -beta,
                     -alpha,
                     depth - 1,
                     transposition_table,
                     iterations,
-                    start_time,
-                )?;
-                let score = -neg_score;
+                    start_time
+                ) {
+                    // found a notable move
+                    SearchResult::BestMove(_, neg_score, state)
+                    | SearchResult::Leaf(neg_score, state) => (
+                        SearchResult::BestMove(chess_move, -neg_score, state),
+                        -neg_score,
+                    ),
+                    SearchResult::CacheHit(neg_score) => {
+                        (SearchResult::CacheHit(-neg_score), -neg_score)
+                    }
 
-                if score >= beta {
-                    return Some((found_state, chess_move, score));
-                }
-                if score > alpha {
-                    best_state = found_state;
-                    best_move = chess_move;
-                    alpha = score;
+                    // out of time
+                    partial @ (SearchResult::OutOfTimeWithResult(_, _)
+                    | SearchResult::OutOfTime) => {
+                        // we should always prefer the earlier value and only return the newly found
+                        // one if it is strictly greater
+                        return match Some(partial).gt(&best_value) {
+                            false => best_value.unwrap_or(partial).timeout(),
+                            true => partial,
+                        };
+                    }
+                };
+
+                if best_value.is_none() || score > best_score {
+                    best_value = Some(findings);
+                    best_score = score;
+
+                    if score > alpha {
+                        alpha = score;
+
+                        if score >= beta {
+                            return findings;
+                        }
+                    }
                 }
             }
 
-            Some((best_state, best_move, alpha))
+            best_value.expect("a value should always be found")
         }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum SearchResult {
+    BestMove(ChessMove, f64, Board),
+    OutOfTimeWithResult(ChessMove, f64),
+    OutOfTime,
+    // unreachable when starting with empty transposition table
+    CacheHit(f64),
+    // unreachable if called with a depth of 1 or greater
+    Leaf(f64, Board),
+}
+
+impl SearchResult {
+    fn score(&self) -> Option<f64> {
+        match *self {
+            Self::OutOfTime => None,
+            Self::BestMove(_, score, _)
+            | Self::OutOfTimeWithResult(_, score)
+            | Self::CacheHit(score)
+            | Self::Leaf(score, _) => Some(score),
+        }
+    }
+
+    fn timeout(&self) -> Self {
+        match *self {
+            Self::BestMove(choice, score, _) | Self::OutOfTimeWithResult(choice, score) => {
+                Self::OutOfTimeWithResult(choice, score)
+            }
+            _ => Self::OutOfTime,
+        }
+    }
+}
+
+impl PartialOrd for SearchResult {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.score()
+            .zip(other.score())
+            .and_then(|(a, b)| a.partial_cmp(&b))
     }
 }
 
@@ -182,12 +262,13 @@ mod test {
     fn dont_give_away_pieces() {
         let position = "r1bqkbnr/2pn2Pp/8/1p6/3PN3/p1P2N2/P4P1P/R1BQKB1R b KQkq - 0 13";
         let board = Board::from_str(position).unwrap();
+        let choice = choose_move(board);
         /*
         println!("{}", evaluate(board.make_move_new(ChessMove::new(Square::F8, Square::G7, None))));
         println!("{}", evaluate(board.make_move_new(ChessMove::new(Square::B5, Square::B4, None))));
         */
         assert_eq!(
-            choose_move(board),
+            choice,
             ChessMove::new(Square::F8, Square::G7, None)
         );
     }
@@ -196,19 +277,15 @@ mod test {
     fn faustyna_please() {
         let position = "rn1k1b1r/pp5p/2p1bp1p/4N3/4P3/1PP5/P3BPPP/RN2K2R w KQ - 0 12";
         let board = Board::from_str(position).unwrap();
-        let mv = choose_move(board);
-        println!("{mv}");
-        assert_ne!(
-            mv,
-            ChessMove::new(Square::H1, Square::F1, None)
-        );
+        let choice = choose_move(board);
+        assert_ne!(choice, ChessMove::new(Square::H1, Square::F1, None));
     }
 
     #[test]
     fn im_losing_my_mind() {
         let position = "8/ppk4p/3R4/7p/8/1PP1K3/P5r1/8 w - - 1 29";
         let board = Board::from_str(position).unwrap();
-        let mv = choose_move(board);
-        assert_ne!(mv, ChessMove::new(Square::D6, Square::D7, None));
+        let choice = choose_move(board);
+        assert_ne!(choice, ChessMove::new(Square::D6, Square::D7, None));
     }
 }
